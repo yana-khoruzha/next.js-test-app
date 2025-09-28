cfd27c751c2ec979ec25071bda0140f8738a1e5c
Yana Khoruzha <i.am.kitiket@gmail.com>
2025-09-29 00:34:13 +0200

    created about, notes, profile pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <header>
+          <h1>Hello Note Hub</h1>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>
+          <p>
+            Created <time dateTime='2025'>2025</time>
+          </p>
+        </footer>
       </body>
     </html>
   );
